44e7da472e2c02582b884e2d20484db1b527c980
leomunhoz <lmpaixao70@gmail.com>
2023-05-14 21:52:21 -0300

    luz, post processing

---
+++ ProjetoEducaV2/Assets/2dAssets/Luz/FlickingLigth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.U2D;
+using Random = UnityEngine.Random;
+
+public class FlickingLigth : MonoBehaviour
+{
+    Light2D tocha;
+  
+    public float timeFlicking;
+    public float minLigthFlickers;
+    public float maxLigthFlickers;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        tocha = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        float intensity = Mathf.Lerp(minLigthFlickers, maxLigthFlickers, Mathf.PingPong(Time.time * timeFlicking, 1f));
+        tocha.pointLightOuterRadius = Random.Range(1.5f, intensity);
+        tocha.shapeLightFalloffSize = Random.Range(1, intensity);
+        
+       
+    }
+
+   
+}
