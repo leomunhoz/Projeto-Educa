7d6aee683b36d0911c79bd073f28b248911e6d3f
leomunhoz <lmpaixao70@gmail.com>
2023-05-18 17:06:28 -0300

    Menu luz, dialogueText

--- ProjetoEducaV2/Assets/Scripts/Dialogue.cs
+++ ProjetoEducaV2/Assets/Scripts/Dialogue.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Dialogue : MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    public int index;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+               textComponent.text = lines[index];
+            }
+        }
+    }
+
+   public void StartDialogue() 
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine() 
+    {
+        if (index < lines.Length -1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            
+        }
+    }
+
+    IEnumerator TypeLine() 
+    {
+        foreach (char c in lines[index].ToCharArray()) 
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
     }
 }
